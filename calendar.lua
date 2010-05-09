25ae51a7a86184a95ab649b7fc31cfb8692a9bcd
Joerg T. (Mic92) <jthalheim@gmail.com>
2010-05-09 10:12:06 +0200

    clean rewrite to work with awful.tooltip

--- calendar.lua
+++ calendar.lua
@@ -1,51 +1,77 @@
-local os = os
-local string = string
-local math = math
-local print = print
-local tostring = tostring
+local os = {date = os.date, time = os.time}
 local markup = require("markup")
+local string = {format = string.format, rep = string.rep}
+local print = print
+local math = {floor = math.floor}
 
 module("calendar")
 
-function display(month,year,weekStart)
-	local t,wkSt=os.time{year=year, month=month+1, day=0},weekStart or 1
-	local d=os.date("*t",t)
-	local mthDays,stDay=d.day,(d.wday-d.day-wkSt+1)%7
+function display(month, year)
 
-	print(mthDays .."\n" .. stDay)
-	local lines = "    "
+    local function fill_line(width)
+        return string.rep(" ", width)
+    end
 
-	for x=0,6 do
-		lines = lines .. os.date("%a ",os.time{year=2006,month=1,day=x+wkSt})
+    local function center(string, width)
+        width = width - #string
+
+        local before = math.floor(width/2)
+        local behind = width - before
+
+        before = fill_line(before)
+        behind = fill_line(behind)
+
+        return before..string..behind
+    end
+
+	local date = os.date("*t", os.time{year=year,month=month+1,day=0})
+    local time = os.time{year=year,month=month,day=1}
+
+    local week = "    "
+    for i=2, 8 do
+	local weekday = os.date("%a ", os.time{year=2006,month=1,day=i})
+		week = week..weekday
+    end
+
+    local max_width = #week
+
+    -- month + year in the header
+    local header = os.date(" %B %Y", time)
+    header = center(header, max_width)
+
+    -- indention of the first line
+    local first_weekday = (date.wday - date.day - 1)%7
+	local body = os.date(" %V", time)..fill_line(first_weekday*4)
+    local position = first_weekday + 1
+
+    -- the rest day of the month
+    for day=1, date.day do
+	local x = day
+	local t = os.time{year=year,month=month,day=day}
+
+	if position == 8 then
+	    body = body.." \n"
+	    -- week of the year
+	    body = body..os.date(" %V",t)
+	    position = 1
 	end
 
-	lines = lines .. "\n" .. os.date(" %V",os.time{year=year,month=month,day=1})
-
-	local writeLine = 1
-	while writeLine < (stDay + 1) do
-		lines = lines .. "    "
-		writeLine = writeLine + 1
+	if os.date("%Y%m%d") == os.date("%Y%m%d", t) then
+	    -- highline current day
+	    x = markup.heading(day)
 	end
 
-	for d=1,mthDays do
-		local x = d
-		local t = os.time{year=year,month=month,day=d}
-		if writeLine == 8 then
-			writeLine = 1
-			lines = lines .. "\n" .. os.date(" %V",t)
-		end
-		if os.date("%Y-%m-%d") == os.date("%Y-%m-%d", t) then
-			x = markup.underline(d)
-		end
-		if (#(tostring(d)) == 1) then
-			x = " " .. x
-		end
-		lines = lines .. "  " .. x
-		writeLine = writeLine + 1
+	if day < 10 then -- <= single character
+	    x = " "..x
 	end
-	local header = os.date("%B %Y\n",os.time{year=year,month=month,day=1})
 
-   return markup.font("monospace", header .. "\n" .. lines)
+	body = ("%s  %s"):format(body, x)
+	position = position + 1
+    end
+
+    body = body..fill_line(max_width - position*4 + 1)
+
+    return markup.normal(markup.font("monospace", ("%s\n%s\n%s"):format(header, week, body)))
 end
 
 -- vim:set filetype=lua fdm=marker tabstop=4 shiftwidth=4 expandtab smarttab autoindent smartindent: --
