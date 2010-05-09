3da2e16cd4dc8e5efdcf88e6e1dfdcced21f1292
Joerg T. (Mic92) <jthalheim@gmail.com>
2010-05-09 10:29:37 +0200

    intial commit

---
+++ markup.lua
@@ -0,0 +1,61 @@
+-- useful markup functions for awesome
+-- found somewhere (wiki maybe?) modularized and current:
+-- by bioe007 perrydothargraveatgmaildotcom
+local beautiful = require("beautiful")
+ 
+-- Markup helper functions
+module("markup")
+ 
+function bg(color, text)
+    if text ~= nil then
+        return '<span bgcolor="'..color..'" >'..text..'</span>'
+    else
+        return ""
+    end
+end
+ 
+function fg(color, text)
+    if text ~= nil then
+        return '<span color="'..color..'">'..text..'</span>'
+    else
+        return ""
+    end
+end
+ 
+function font(font, text)
+    if text ~= nil then
+        return '<span font_desc="'..font..'">'..text..'</span>'
+    else
+        return ""
+    end
+end
+ 
+function normal(t)
+    return bg(beautiful.bg_normal, fg(beautiful.fg_normal, t))
+end
+ 
+function focus(t)
+    return bg(beautiful.bg_focus, fg(beautiful.fg_focus, t))
+end
+ 
+function urgent(t)
+    return bg(beautiful.bg_urgent, fg(beautiful.fg_urgent, t))
+end
+ 
+function bold(text)
+    return '<b>'..text..'</b>'
+end
+
+function underline(text)
+    return '<u>'..text..'</u>'
+end
+
+function italic(text)
+    return '<i>'..text..'<i>'
+end
+ 
+function heading(text)
+    return fg(beautiful.fg_focus, bold(text))
+end
+ 
+-- vim:set filetype=lua fdm=marker tabstop=4 shiftwidth=4 expandtab smarttab autoindent smartindent: --
