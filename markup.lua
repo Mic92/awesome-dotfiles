aa66cc112f0e900980cd604c8c50e424c34747ac
Joerg T. (Mic92) <jthalheim@gmail.com>
2011-02-17 20:07:47 +0100

    new gradient function

--- markup.lua
+++ markup.lua
@@ -2,31 +2,32 @@
 -- found somewhere (wiki maybe?) modularized and current:
 -- by bioe007 perrydothargraveatgmaildotcom
 local beautiful = require("beautiful")
- 
+local tonumber = tonumber
+local string_format = string.format
 -- Markup helper functions
 module("markup")
- 
+
 function bg(color, text)
-    if text ~= nil then
-        return '<span bgcolor="'..color..'" >'..text..'</span>'
+    if color ~= nil then
+        return '<span bgcolor="'..color..'" >'..(text or "")..'</span>'
     else
-        return ""
+        return text
     end
 end
  
 function fg(color, text)
-    if text ~= nil then
-        return '<span color="'..color..'">'..text..'</span>'
+    if color ~= nil then
+        return '<span color="'..color..'">'..(text or "")..'</span>'
     else
-        return ""
+        return text
     end
 end
  
 function font(font, text)
-    if text ~= nil then
-        return '<span font_desc="'..font..'">'..text..'</span>'
+    if color ~= nil then
+        return '<span font_desc="'..font..'">'..(text or "")..'</span>'
     else
-        return ""
+        return text 
     end
 end
  
@@ -51,11 +52,34 @@ function underline(text)
 end
 
 function italic(text)
-    return '<i>'..(text or "")..'<i>'
+    return '<i>'..(text or "")..'</i>'
 end
  
 function heading(text)
     return fg(beautiful.fg_focus, bold(text))
+end
+
+function gradient(color, to_color, min, max, value)
+    local function color2dec(c)
+        return tonumber(c:sub(2,3),16), tonumber(c:sub(4,5),16), tonumber(c:sub(6,7),16)
+    end
+
+    local factor = 0
+    if (value >= max ) then 
+        factor = 1  
+    elseif (value > min ) then 
+        factor = (value - min) / (max - min)
+    end 
+
+    local red, green, blue = color2dec(color) 
+    local to_red, to_green, to_blue = color2dec(to_color) 
+
+    red   = red   + (factor * (to_red   - red))
+    green = green + (factor * (to_green - green))
+    blue  = blue  + (factor * (to_blue  - blue))
+
+    -- dec2color
+    return string_format("#%02x%02x%02x", red, green, blue)
 end
  
 -- vim:set filetype=lua fdm=marker tabstop=4 shiftwidth=4 expandtab smarttab autoindent smartindent: --
