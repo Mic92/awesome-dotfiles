c411ad58413eb045f2b26299eda271daca6d89b9
Jörg Thalheim <jthalheim@gmail.com>
2012-02-24 14:48:14 +0100

    Add keybinding to restore a client

--- rc.lua
+++ rc.lua
@@ -757,6 +757,7 @@ local globalkeys = awful.util.table.join(
   awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
   awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
   awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+  awful.key({ modkey, "Control" }, "n", awful.client.restore),
   --}}
 
   -- {{{ Custom Bindings
