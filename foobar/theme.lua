24001c4f074e45b41825fb1ff7ae056cdb726efa
Jörg Thalheim <joerg@higgsboson.tk>
2014-03-30 11:19:57 +0200

    use old awesome background

--- foobar/theme.lua
+++ foobar/theme.lua
@@ -83,7 +83,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themedir .. "/background.jpg"
+theme.wallpaper = themedir .. "/background.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
